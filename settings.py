7354c64dae8acc8d856dc835aeb038e8e76f9d29
Jack DiMassimo <jvdimas@gmail.com>
2011-02-17 14:23:37 -0600

    added settings file, removed user token info from it

---
+++ settings.py
@@ -0,0 +1,9 @@
+###############################
+# Settings
+###############################
+# App-Specific
+CONSUMER_TOKEN = "Ev7Rv3mS9liUCeIgEcYYw"
+CONSUMER_SECRET = "mwRSnOQZpsH5uct017ATlOPSbVLD2UCkOG1Fc3ntuMc"
+
+# MongoDB name
+DB_NAME = "tweets"
