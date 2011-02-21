9cfc5ad40e5829e722c3edf5fc0c816fd4e352b1
Jack DiMassimo <jvdimas@gmail.com>
2011-02-21 13:18:30 -0600

    Readme update

---
+++ analyze/AnalyzerPercentEnglish.py
@@ -0,0 +1,17 @@
+import TweetAnalyzer
+
+class AnalyzerPercentEnglish(TweetAnalyzer.TweetAnalyzer):
+  def score_tweet(self, tweet):
+    if tweet[4] == 'en':
+      return 1
+    else:
+      return 0
+
+  def average_scores(self, scores):
+    n = 0
+    n_en = 0
+    for s in scores:
+      n += 1
+      n_en += s
+
+    return float(n_en)/n
