9e54e944982044f171bf06cf52d37e94c8f0106b
Jack DiMassimo <jvdimas@gmail.com>
2011-03-11 14:18:58 -0600

    r file updated

--- analyze.r
+++ analyze.r
@@ -1,9 +1,18 @@
 # Reads csv data created by analyze.py and makes graphs
 
 # Read csv data
-t <- read.csv(file="out.csv")
+# t <- read.csv(file="out.csv")
 
 # Plot density info
-png('graphs/density.png', width=1600)
-plot(t,ylab='score', xlab='datetime')
+# png('graphs/density.png', width=1600)
+# plot(t,ylab='score', xlab='datetime')
+# dev.off()
+
+t <- read.csv(file="csv/combine.csv")
+
+# Write to png
+png('graphs/densities.png', width=1600, height=960)
+par(mfrow=c(2,1))
+plot(t$date, t$total, ylab="Number of Tweets", xlab="Datetime (utc)", main="Total Number of Tweets Received")
+plot(t$date, t$english, ylab="% English", xlab="Datetime (utc)", main="% Tweets English")
 dev.off()
